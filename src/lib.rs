0a433d7fb61754dcadbb443a4a14b6d255647fc4
Ugur-Arikan <uurfun@gmail.com>
2023-09-14 22:59:41 +0200

    documentation update
    in addition
    
    * mod prelude is added
    * crate category is fixed

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,22 @@
 //! Priority queue traits; d-ary heap implementations having binary heap as a special case.
 
+#![warn(
+    missing_docs,
+    clippy::unwrap_in_result,
+    clippy::unwrap_used,
+    clippy::panic,
+    clippy::panic_in_result_fn,
+    clippy::float_cmp,
+    clippy::float_cmp_const,
+    clippy::missing_panics_doc,
+    clippy::todo
+)]
+
 mod dary;
 mod has_index;
 mod positions;
+/// Common traits, structs and enums.
+pub mod prelude;
 mod priority_queue;
 mod priority_queue_deckey;
 
