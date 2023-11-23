f5a871725b1d8e62ef704accdc03859779834678
Ugur-Arikan <uurfun@gmail.com>
2023-11-23 19:35:22 +0100

    documentation updated

--- src/lib.rs
+++ src/lib.rs
@@ -4,17 +4,17 @@
 //!
 //! 1. To provide priority queue traits for two goals:
 //!     * to separate simple and decrease key queues, and
-//!     * to enable developing functions or algorithms which are generic over the actual queue type.
-//! 2. To provide d-ary heap implementations, which is generic over `d`:
+//!     * to enable developing functions or algorithms which are generic over the actual queue implementation.
+//! 2. To provide d-ary heap implementation that is generic over `d`:
 //!     * the implementation makes use of the const-generics which conveniently allows to take advantage of bit operations for the cases where `d` is a power of two, while having a general implementation for others.
 //!
 //!
 //! ## Traits
 //!
-//! This crate defines two priority queue traits for (node, key) pairs with the following features:
+//! Two priority queue traits are defined with the following features:
 //!
-//! * `PriorityQueue<N, K>`: providing basic priority queue functionalities.
-//! * `PriorityQueueDecKey<N, K>`: extends the `PriorityQueue` by allowing methods to mutate keys of existing items.
+//! * `PriorityQueue<N, K>`: providing basic priority queue functionalities, such as `push` and `pop`.
+//! * `PriorityQueueDecKey<N, K>`: extends the `PriorityQueue<N, K>` by allowing methods to mutate keys of existing items, such as the `decrease_key` operation.
 //!
 //! The differences can be summarized as follows:
 //!
@@ -28,30 +28,29 @@
 //!
 //! This crate provides three kinds of queue implementations. You may find below their differences and the situations each could be a better fit.
 //!
-//! * `OrxDaryHeap` is a simple `PriorityQueue` implemented as a heap.
-//!     * Benefits from its simplicity.
-//!     * Could be considered as the default queue implementation where the memory efficiency is not very critical.
-//! * `OrxDaryHeapOfIndices` is a `PriorityQueueDecKey` which aims to be performant while requiring the items that can enter the queue to implement `HasIndex` trait, which simply has the single method `fn index(&self) -> usize`. The implementation is a heap parallel to a mapping of items to positions on the heap.
+//! * `DaryHeap` is a simple `PriorityQueue` implemented as a heap.
+//!     * It is simple and could be considered as the default queue implementation where the space complexity is not very critical.
+//! * `DaryHeapOfIndices` is a `PriorityQueueDecKey` which aims to be performant while requiring the items that can enter the queue to implement `HasIndex` trait, which simply has the single method `fn index(&self) -> usize`. The implementation is a heap parallel to a mapping of items to positions on the heap.
 //!     * Particularly useful in performance critical algorithms which additionally requires memory efficiency.
-//!     * Requires that the size of the closed set of candidates that can enter the queue is known. This is not a strong limitation in many algorithms. For instance, for most network traversal algorithms, this equals to the number of nodes in the graph.
-//! * `OrxDaryHeapWithMap` is also a `PriorityQueueDecKey` which aims to be flexible requiring the items to implement `Hash + Eq`. It has a similar implementation with the prior except that it utilizes a map to keep track of item positions on the heap.
-//!     * This can be considered as the alternative to `OrxDaryHeapOfIndices` in memory critical situations, where it is not possible to satisfy the requirement on knowing the size of the closed set of candidates.
+//!     * Requires the size of the closed set of candidates that can enter the queue to be known. This is not a strong limitation in many algorithms. For instance, for most network traversal algorithms, the cardinality of the candidates set is the number of nodes in the graph.
+//! * `DaryHeapWithMap` is also a `PriorityQueueDecKey` which aims to be flexible requiring the items to implement `Hash + Eq`. It has a similar implementation with the prior except that it utilizes a map to keep track of item positions on the heap.
+//!     * This can be considered as the alternative to `DaryHeapOfIndices` in memory critical situations, where it is not possible to satisfy the requirement on knowing the size of the closed set of candidates.
 //!
-//! Note that each of the above kinds is generic over `d`, leading to a large number of possible queue choices. The traits come handy here allowing to write algorithms generic over the queue, which then can be benchmarked with different concrete types to find the most performant implementation for the problem. In the next section, we summarize such an experiment.
+//! Note that each of the above kinds is generic over `d`, leading to a large number of possible queue choices. The aforementioned traits become useful here allowing to write algorithms generic over the queue. These algorithms can then be benchmarked with different concrete types to find the most performant implementation for the problem. In the next section, we summarize such an experiment.
 //!
 //! ### Experiments on Shortest Path Algorithm
 //!
-//! Using the traits, we can write algorithms which are generic over the queues. This allows to conveniently benchmark over the relevant data to find the best fitting queue implementation. You may see such an example exercise in the repository [https://github.com/orxfun/orx-bench-shortest-path](https://github.com/orxfun/orx-bench-shortest-path) which allows to compare different shortest path algorithm implementations. From our current experiments using random and real life networks, the following table can be consulted.
+//! Using the traits, we can write algorithms which are generic over the queues. This allows to conveniently benchmark to find the best fitting queue implementation. You may see such an example exercise in the repository [https://github.com/orxfun/orx-bench-shortest-path](https://github.com/orxfun/orx-bench-shortest-path) which allows to compare different shortest path algorithm implementations. From the last experiments using random and real life networks, the following table can be consulted.
 //!
 //! |                    |     |     | Queue Trait             | Queue Type               |
 //! |--------------------|-----|-----|-------------------------|--------------------------|
 //! | Memory Critical?   | yes |     |  `PriorityQueueDecKey`  |                          |
-//! | - Is Graph Sparse? |     | yes |                         |  `OrxDaryHeapOfIndices`  |
-//! |                    |     | no  |                         |  `OrxDaryHeapOfIndices`  |
+//! | - Is Graph Sparse? |     | yes |                         |  `DaryHeapOfIndices`  |
+//! |                    |     | no  |                         |  `DaryHeapOfIndices`  |
 //! |.                   |     |     |                         |                          |
 //! | Memory Critical?   | no  |     |  `PriorityQueue`        |                          |
-//! | - Is Graph Sparse? |     | yes |                         |  `OrxDaryHeap`           |
-//! |                    |     | no  |                         |  `OrxDaryHeap` or `std::collections::BinaryHeap` |
+//! | - Is Graph Sparse? |     | yes |                         |  `DaryHeap`           |
+//! |                    |     | no  |                         |  `DaryHeap` or `std::collections::BinaryHeap` |
 //!
 //!
 //! ## Example
@@ -59,6 +58,7 @@
 //! ```rust
 //! use orx_priority_queue::*;
 //!
+//! // generic over simple priority queues
 //! fn test_priority_queue<P>(mut pq: P)
 //! where
 //!     P: PriorityQueue<usize, f64>,
@@ -82,6 +82,8 @@
 //!         println!("pop {:?}", popped);
 //!     }
 //! }
+//!
+//! // generic over decrease-key priority queues
 //! fn test_priority_queue_deckey<P>(mut pq: P)
 //! where
 //!     P: PriorityQueueDecKey<usize, f64>,
@@ -125,21 +127,9 @@
 //! // Binary, Ternary or Quarternary to fix D of Dary
 //! test_priority_queue(BinaryHeap::default());
 //! test_priority_queue(BinaryHeapWithMap::default());
-//! test_priority_queue(BinaryHeapOfIndices::with_index_bound(100));
-//! test_priority_queue_deckey(BinaryHeapWithMap::default());
-//! test_priority_queue_deckey(BinaryHeapOfIndices::with_index_bound(100));
-//!
-//! test_priority_queue(TernaryHeap::default());
-//! test_priority_queue(TernaryHeapWithMap::default());
-//! test_priority_queue(TernaryHeapOfIndices::with_index_bound(100));
+//! test_priority_queue(BinaryHeapOfIndices::with_upper_limit(100));
 //! test_priority_queue_deckey(TernaryHeapWithMap::default());
-//! test_priority_queue_deckey(TernaryHeapOfIndices::with_index_bound(100));
-//!
-//! test_priority_queue(QuarternaryHeap::default());
-//! test_priority_queue(QuarternaryHeapWithMap::default());
-//! test_priority_queue(QuarternaryHeapOfIndices::with_index_bound(100));
-//! test_priority_queue_deckey(QuarternaryHeapWithMap::default());
-//! test_priority_queue_deckey(QuarternaryHeapOfIndices::with_index_bound(100));
+//! test_priority_queue_deckey(QuarternaryHeapOfIndices::with_upper_limit(100));
 //! ```
 //!
 //! ## License
